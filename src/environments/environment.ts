09210a9f65124c763fa3b7630d7fa86c6e20ade2
juanpiw <juanpablojpw@gmail.com>
2025-10-03 06:49:49 -0300

    apuntando a prod en el baseurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000',
+  apiBaseUrl: 'https://adomi.impactrenderstudio.com',
   stripePublishableKey: 'pk_test_51234567890abcdef' // Reemplazar con tu clave real
 };
