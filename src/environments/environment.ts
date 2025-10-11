7844d86aae72d53e51b37dd581f02ed9b72149c0
juanpiw <juanpablojpw@gmail.com>
2025-10-11 13:11:11 -0300

    reemplzar clave publica

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://adomi.impactrenderstudio.com',
-  stripePublishableKey: 'pk_test_51234567890abcdef', // Reemplazar con tu clave real
+  stripePublishableKey: 'pk_live_51Opto6Lae2ozUqcfkEx6dKK4p8jRPlibbXBIfsTqg8KKNXQzIyDhNO0Ok0Y1uhdGw8QXglvcyJowQh7PuKS9b5db00ddVgUejq', // Reemplazar con tu clave real
   googleClientId: '319925819744-4snhnq7vtcb33g516a67qu7bu42r2hk1.apps.googleusercontent.com',
   googleRedirectUri: 'http://localhost:4200/auth/google/callback',
   // Agrega tu API Key real para activar Google Maps en el componente ui-map-view
