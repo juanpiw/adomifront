9259f66e40e41fef9cbf9deac43de46842520a2d
juanpiw <juanpablojpw@gmail.com>
2025-10-18 13:26:23 -0300

    push

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_AjqYAWsHbYPydVfV0NHXfQKv1RSrbn8",
+  authDomain: "adomiapp-notificaciones.firebaseapp.com",
+  projectId: "adomiapp-notificaciones",
+  storageBucket: "adomiapp-notificaciones.firebasestorage.app",
+  messagingSenderId: "584385142195",
+  appId: "1:584385142195:web:9de53f99549d9fefb2bfc6",
+  measurementId: "G-ZV20C8NHXY",
+  // VAPID key (Web Push) - obtener de Firebase Console > Project Settings > Cloud Messaging > Web Push certificates
+  vapidKey: "YOUR_VAPID_KEY_HERE" // TODO: Reemplazar con tu VAPID key
+};
+
