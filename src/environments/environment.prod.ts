58daa02d31d189cb7d1f8d1d0afc58ae515690f4
juanpiw <juanpablojpw@gmail.com>
2025-10-27 11:29:23 -0300

    cambio de claves

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://adomi.impactrenderstudio.com',
-  stripePublishableKey: 'pk_test_51SKHttLQigVjRMUbKJnseEuIUaRBp4czzoSbgeMXpAqfRcf3QlWxpmy3TBCmySOuOv813RiH0NWljtQqJG2TmzDV00GWVMJZki', // TEST
+  stripePublishableKey: 'pk_test_51Opto6Lae2ozUqcf5gI2CBX5cE1uZOAZebL8OO9wba6LVKFiW48hhFDVuDNVHH9iXhPKyLlJEonRApl7RGkHm1mh00jPkenSNZ', // TEST
   googleClientId: '319925819744-4snhnq7vtcb33g516a67qu7bu42r2hk1.apps.googleusercontent.com',
   googleRedirectUri: 'https://adomi.impactrenderstudio.com/auth/google/callback',
   googleMapsApiKey: 'AIzaSyDWOGEJ9XnCyrKDSvpK4TKFj1_eM3NHdeo'
