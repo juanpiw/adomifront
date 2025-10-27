3978e126a523c17f663c020e07d049d9e79137ae
juanpiw <juanpablojpw@gmail.com>
2025-10-27 17:10:38 -0300

    envarioment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://adomi.impactrenderstudio.com',
-  stripePublishableKey: 'pk_test_51SKHttLQigVjRMUbKJnseEuIUaRBp4czzoSbgeMXpAqfRcf3QlWxpmy3TBCmySOuOv813RiH0NWljtQqJG2TmzDV00GWVMJZki', // TEST
+  stripePublishableKey: 'pk_test_51SMwb8LqBmLxjDKEBO7WzPjK6dL2uYhHdN5FhfqtCYSadKt4nNbpk07ffgOgMu9PbKd7smFB1u8nq4bbMUPgnoTI00TJyeWOfI', // TEST
   connectEnabled: false,
   googleClientId: '319925819744-4snhnq7vtcb33g516a67qu7bu42r2hk1.apps.googleusercontent.com',
   googleRedirectUri: 'https://adomi.impactrenderstudio.com/auth/google/callback',
