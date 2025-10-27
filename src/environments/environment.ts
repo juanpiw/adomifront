1c3ff3a64f9c883e496383044268bb8d7a80f2c7
juanpiw <juanpablojpw@gmail.com>
2025-10-26 21:07:32 -0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://adomi.impactrenderstudio.com',
-  stripePublishableKey: 'pk_live_51Opto6Lae2ozUqcfkEx6dKK4p8jRPlibbXBIfsTqg8KKNXQzIyDhNO0Ok0Y1uhdGw8QXglvcyJowQh7PuKS9b5db00ddVgUejq', // Reemplazar con tu clave real
+  stripePublishableKey: 'pk_test_XXXXXXXXXXXXXXXXXXXXXXXXXXXX', // MODO TEST
   googleClientId: '319925819744-4snhnq7vtcb33g516a67qu7bu42r2hk1.apps.googleusercontent.com',
   googleRedirectUri: 'http://localhost:4200/auth/google/callback',
   // API Key de Google Maps (usar clave restringida por dominio)
