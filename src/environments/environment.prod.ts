a5d3baab69dfa0f0d85ad647fc0a6bf174870614
juanpiw <juanpablojpw@gmail.com>
2026-01-19 15:08:45 -0300

    data layer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { firebaseConfig } from './firebase.config';
 export const environment = {
   production: true,
   apiBaseUrl: 'https://adomi.impactrenderstudio.com',
-  gaMeasurementId: '',
+  gaMeasurementId: 'G-ZQYRWPHT60',
   stripePublishableKey: 'pk_test_51SMwb8LqBmLxjDKEBO7WzPjK6dL2uYhHdN5FhfqtCYSadKt4nNbpk07ffgOgMu9PbKd7smFB1u8nq4bbMUPgnoTI00TJyeWOfI', // TEST
   connectEnabled: false,
   googleClientId: '319925819744-4snhnq7vtcb33g516a67qu7bu42r2hk1.apps.googleusercontent.com',
